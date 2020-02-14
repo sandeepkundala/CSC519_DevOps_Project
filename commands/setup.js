ff76702b67fc8cb9455ac570a8b45eeccdcb1d7c
skundal <skundal@ncsu.edu>
2020-02-14 12:09:18 -0500

    changed name to jenkins server

--- commands/setup.js
+++ commands/setup.js
@@ -37,8 +37,8 @@ async function run(privateKey) {
     let result = child.spawnSync(`bakerx`, `run ansible-srv bionic --ip 192.168.33.10 --sync`.split(' '), {shell:true, stdio: 'inherit'} );
     if( result.error ) { console.log(result.error); process.exit( result.status ); }
 
-    console.log(chalk.blueBright('Provisioning mattermost server...'));
-    result = child.spawnSync(`bakerx`, `run mattermost-srv bionic --ip 192.168.33.80`.split(' '), {shell:true, stdio: 'inherit'} );
+    console.log(chalk.blueBright('Provisioning jenkins server...'));
+    result = child.spawnSync(`bakerx`, `run jenkins-srv bionic --ip 192.168.33.80`.split(' '), {shell:true, stdio: 'inherit'} );
     if( result.error ) { console.log(result.error); process.exit( result.status ); }
 
     console.log(chalk.blueBright('Installing privateKey on configuration server'));
