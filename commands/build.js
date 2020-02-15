1e4e8464fcead28c23119780dc006e56cec6868b
skundal <skundal@ncsu.edu>
2020-02-15 00:13:34 -0500

    checkbox.io init

---
+++ commands/build.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const path = require('path');
+const chalk = require('chalk');
+const sshSync = require('../lib/ssh');
+const inventoryPath = '/bakerx/cm/inventory.ini';
+
+exports.command = 'build <job>';
+exports.desc = 'Build job';
+exports.builder = yargs => {
+    yargs.options({
+    });
+};
+
+exports.handler = async argv => {
+    const { job } = argv;
+    (async () => {
+        if (fs.existsSync(path.resolve('cm/'+job+'.yml'))){
+            await run(job);
+        }
+        else{
+            console.error("Job file doesn't exist");
+        }
+    })();
+};
+
+async function run(job){
+    let filePath = '/bakerx/cm/'+job+'.yml';
+    console.log(chalk.blueBright('Running build environment...'));
+    let result = sshSync(`/bakerx/cm/run-ansible.sh ${filePath} ${inventoryPath}`, 'vagrant@192.168.33.10');
+    if( result.error ) { process.exit( result.status ); }
+}
+
