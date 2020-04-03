958f1646db3d8693180c33c095bc42a6ae33276e
Sandeep Kundala <skundal@ncsu.edu>
2020-04-03 22:00:45 +0530

    Update fuzzer.js

--- Fuzzer/fuzzer.js
+++ Fuzzer/fuzzer.js
@@ -50,7 +50,7 @@ function createRandomChangesInAFile(filePath) {
     
     lines.forEach(function(line) {
 
-        if (randomizer.bool(0.25)) {
+        if (randomizer.bool()) {
             if (line.match(">") && !line.match("->")) {
                 line = line.replace(/>/g, "<");
             }
@@ -60,7 +60,7 @@ function createRandomChangesInAFile(filePath) {
             }
         }
 
-        if (randomizer.bool(0.25)){
+        if (randomizer.bool()){
             if (line.match("!=")){
                 line=line.replace(/!=/g,'==');
             }
@@ -69,7 +69,7 @@ function createRandomChangesInAFile(filePath) {
             }
         }
 
-        if (randomizer.bool(0.25)){
+        if (randomizer.bool()){
             if (line.match(/[0]/)){
                 line = line.replace(/[0]/g, "1");
             }
@@ -78,7 +78,7 @@ function createRandomChangesInAFile(filePath) {
             }
         }
 
-        if (randomizer.bool(0.25)){
+        if (randomizer.bool()){
             if (line.match(/"[\w\d\s]*"/g)){
                 var stringVar = line.match(/"[\w\d\s]*"/g);
                 stringVar.forEach(function(string){
