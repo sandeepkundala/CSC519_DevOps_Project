e085eeb72b461368f9e2f8a572926bd4e1a42016
skundal <skundal@ncsu.edu>
2020-04-12 12:25:33 +0530

    BUGFIX - Replace /home/vagrant with variable

--- cm/roles/create_jobs/templates/iTrustBuild.Unix.sh
+++ cm/roles/create_jobs/templates/iTrustBuild.Unix.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/vagrant
+cd $JENKINS_HOME_PATH
 sudo rm -rf iTrust2-v6
 sudo git clone https://$GH_USER:$GH_PASS@github.ncsu.edu/engr-csc326-staff/iTrust2-v6.git
 cd iTrust2-v6/iTrust2/src/main/java
@@ -7,8 +7,8 @@ sudo touch email.properties
 sudo touch db.properties
 sudo rm db.properties.template
 sudo rm email.properties.template
-sudo cp /home/vagrant/db.properties.j2 /home/vagrant/iTrust2-v6/iTrust2/src/main/java/db.properties
-sudo cp /home/vagrant/email.properties.j2 /home/vagrant/iTrust2-v6/iTrust2/src/main/java/email.properties
-cd /home/vagrant/iTrust2-v6/iTrust2/
+sudo cp $JENKINS_HOME_PATH/db.properties.j2 $JENKINS_HOME_PATH/iTrust2-v6/iTrust2/src/main/java/db.properties
+sudo cp $JENKINS_HOME_PATH/email.properties.j2 $JENKINS_HOME_PATH/iTrust2-v6/iTrust2/src/main/java/email.properties
+cd $JENKINS_HOME_PATH/iTrust2-v6/iTrust2/
 sudo mvn -f pom-data.xml process-test-classes
 sudo mvn clean test verify org.apache.maven.plugins:maven-checkstyle-plugin:3.1.0:checkstyle
