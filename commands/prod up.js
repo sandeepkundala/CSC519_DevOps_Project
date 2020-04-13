2ddbac6bb07e1f634956c94e95485530574832b0
skundal <skundal@ncsu.edu>
2020-04-13 20:22:18 +0530

    Provision environment

--- commands/prod up.js
+++ commands/prod up.js
@@ -2,6 +2,8 @@ const child = require('child_process');
 const chalk = require('chalk');
 const path = require('path');
 const os = require('os');
+const sshSync = require('../lib/ssh');
+const inventoryPath = '/bakerx/cm/inventory.ini';
 
 exports.command = 'prod up'
 exports.desc = 'Provision cloud instances and control plane'
@@ -15,6 +17,9 @@ exports.handler = async argv => {
 };
 
 async function run(){
+
+    console.log(chalk.greenBright('Configure server to provide instances in AWS'));
+    let result = sshSync(`ansible-playbook /bakerx/provision/provision.yml -i ${inventoryPath}`, 'vagrant@192.168.33.10');
 
     console.log(chalk.greenBright('Creating new key pair'));
 
