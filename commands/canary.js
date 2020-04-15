d5f4819622855bbb9605c9e353c38365a4b7953e
Rajshree Jain <rjain27@ncsu.edu>
2020-04-15 18:16:14 -0400

    canary start

--- commands/canary.js
+++ commands/canary.js
@@ -1,30 +1,62 @@
-const fs = require('fs');
-const path = require('path');
-const chalk = require('chalk');
-const sshSync = require('../lib/ssh');
+const fs = require("fs");
+const path = require("path");
+const chalk = require("chalk");
+const sshSync = require("../lib/ssh");
+const child = require("child_process");
 
-exports.command = 'canary <blueBranch> <greenBranch>';
-exports.desc = 'Deploy the application in cloud servers';
-exports.builder = yargs => {
-    yargs.options({
-    });
+exports.command = "canary <blueBranch> <greenBranch>";
+exports.desc = "Spin up 3 local machines";
+
+exports.builder = (yargs) => {
+  yargs.options({});
 };
 
-exports.handler = async argv => {
-    const { blueBranch, greenBranch } = argv;
-    (async () => {
-        if (blueBranch != null  && greenBranch != null){
-            await run(blueBranch, greenBranch);
-            // console.log(g);
-        }
-        else{
-            console.error("Arguments missing");
-        }
-    })();
+exports.handler = async (argv) => {
+  const { blueBranch, greenBranch } = argv;
+  (async () => {
+    if (blueBranch != null && greenBranch != null) {
+      await run(blueBranch, greenBranch);
+      // console.log(g);
+    } else {
+      console.error("Arguments missing");
+    }
+  })();
 };
 
-async function run(blueBranch, greenBranch){
-    console.log(blueBranch);
-    console.log(greenBranch);
-    
+async function run(blueBranch, greenBranch) {
+  console.log(blueBranch);
+  console.log(greenBranch);
+
+  console.log(chalk.blueBright("Provisioning one server..."));
+  let result = child.spawnSync(
+    `bakerx`,
+    `run master bionic --ip 192.168.33.30 --sync --memory 3072`.split(" "),
+    { shell: true, stdio: "inherit" }
+  );
+  if (result.error) {
+    console.log(result.error);
+    process.exit(result.status);
+  }
+
+  console.log(chalk.blueBright("Provisioning two server..."));
+  result = child.spawnSync(
+    `bakerx`,
+    `run broken bionic --ip 192.168.33.40 --memory 3072`.split(" "),
+    { shell: true, stdio: "inherit" }
+  );
+  if (result.error) {
+    console.log(result.error);
+    process.exit(result.status);
+  }
+
+  console.log(chalk.blueBright("Provisioning three server..."));
+  result = child.spawnSync(
+    `bakerx`,
+    `run monitor bionic --ip 192.168.33.50 --memory 3072`.split(" "),
+    { shell: true, stdio: "inherit" }
+  );
+  if (result.error) {
+    console.log(result.error);
+    process.exit(result.status);
+  }
 }
