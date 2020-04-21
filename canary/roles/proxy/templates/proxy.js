0e3621d0a9c22a25a89c23badd5e98a4be647c78
Rajshree Jain <rjain27@ncsu.edu>
2020-04-21 13:50:35 -0400

    message

--- canary/roles/proxy/templates/proxy.js
+++ canary/roles/proxy/templates/proxy.js
@@ -3,7 +3,7 @@ var httpProxy = require("http-proxy");
 var request = require("request");
 var fs = require("fs");
 const child = require("child_process");
-const redis = require('redis');
+const redis = require("redis");
 
 let count = 0;
 let time = 0;
@@ -16,10 +16,9 @@ let green_ip = "192.168.33.40";
 console.log(blue_ip);
 console.log(green_ip);
 
-var servers =
-[
-	{name: "blue", url: `http://${blue_ip}:3000`, status: 0, scoreTrend : []},
-	{name: "green", url: `http://${green_ip}:3000`, status: 0,  scoreTrend : []}
+var servers = [
+  { name: "blue", url: `http://${blue_ip}:3000`, status: 0, scoreTrend: [] },
+  { name: "green", url: `http://${green_ip}:3000`, status: 0, scoreTrend: [] },
 ];
 
 function fileread(filename) {
@@ -37,29 +36,29 @@ const options = {
   },
 };
 
-function updateHealth(server)
-{
-	let score = 0;
+function updateHealth(server) {
+  let score = 0;
 
-	score += server.memoryLoad < 25.00 ? 1: 0;
-	score += server.cpu < 25.00 ? 1: 0;
-	score += server.latency < 100 ? 1: 0;
-  score += server.status === 200 ? 1: 0;
+  score += server.memoryLoad < 25.0 ? 1 : 0;
+  score += server.cpu < 25.0 ? 1 : 0;
+  score += server.latency < 100 ? 1 : 0;
+  score += server.status === 200 ? 1 : 0;
 
   server.scoreTrend.push(score);
-
+  console.log(server.memoryLoad);
+  console.log(server.cpu);
+  console.log(server.latency);
+  console.log(server.status);
 }
 
 function main() {
-
   var data = fileread("/home/vagrant/resources/survey.json");
   var post_req = http.request(options, function (res) {
     res.setEncoding("utf8");
     console.log("Response from backend is " + res.statusCode);
-    if (time < 5000){
+    if (time < 5000) {
       servers[0].status = res.statusCode;
-    }
-    else{
+    } else {
       servers[1].status = res.statusCode;
     }
     res.on("data", function (chunk) {});
@@ -68,28 +67,24 @@ function main() {
   post_req.write(data);
   post_req.end();
 
-  let client = redis.createClient(6379, 'localhost', {});
-  for( var server of servers )
-	{
-		// The name of the server is the name of the channel to recent published events on redis.
-		client.subscribe(server.name);
+  let client = redis.createClient(6379, "localhost", {});
+  for (var server of servers) {
+    // The name of the server is the name of the channel to recent published events on redis.
+    client.subscribe(server.name);
   }
   // When an agent has published information to a channel, we will receive notification here.
-	client.on("message", function (channel, message)
-	{
-		console.log(`Received message from agent: ${channel}`)
-		for( var server of servers )
-		{
-			// Update our current snapshot for a server's metrics.
-			if( server.name == channel)
-			{
-				let payload = JSON.parse(message);
-				server.memoryLoad = payload.memoryLoad;
-				server.cpu = payload.cpu;
-				updateHealth(server);
-			}
-		}
-	});
+  client.on("message", function (channel, message) {
+    console.log(`Received message from agent: ${channel}`);
+    for (var server of servers) {
+      // Update our current snapshot for a server's metrics.
+      if (server.name == channel) {
+        let payload = JSON.parse(message);
+        server.memoryLoad = payload.memoryLoad;
+        server.cpu = payload.cpu;
+        updateHealth(server);
+      }
+    }
+  });
 }
 
 http
@@ -113,4 +108,3 @@ var heartbeatTimer = setInterval(function () {
   main();
   time = Date.now() - now;
 }, 500);
-
